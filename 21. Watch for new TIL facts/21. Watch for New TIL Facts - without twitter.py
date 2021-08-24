84d11d2a0af039450395ecf3669d67c70b652a3a
RandomUser171 <47086691+RandomUser171@users.noreply.github.com>
2021-08-24 19:01:05 +0200

    Update 21. Watch for New TIL Facts - without twitter.py

--- 21. Watch for new TIL facts/21. Watch for New TIL Facts - without twitter.py
+++ 21. Watch for new TIL facts/21. Watch for New TIL Facts - without twitter.py
@@ -2,7 +2,7 @@ import json
 import urllib3
 from class_latest_fact import *
 
-ouga = Latest_fact()
-print(ouga.title())
-print(ouga.link())
+lFact = Latest_fact()
+print(lFact.title())
+print(lFact.link())
 
