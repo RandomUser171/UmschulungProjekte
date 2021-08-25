9df2be6a80f0ad6176a4e2f46c4be6076aec33da
RandomUser171 <47086691+RandomUser171@users.noreply.github.com>
2021-08-25 18:35:19 +0200

    Update 3. Pythagorean Triples Checker.py

--- 3. Pythagorean Triples Checker.py
+++ 3. Pythagorean Triples Checker.py
@@ -18,19 +18,27 @@ def user_input():
 # Asks the user for the 3 sides of the triangle
 # and sorts the hypotenuse (c)
 def questionPrompter():
+    
     print('Please enter the first side of the triangle:')
     a = abs(user_input())
     print('Please enter the second side of the triangle:')
     b = abs(user_input())
     print('Please enter the third side of the triangle:')
     c = abs(user_input())
+    
+    
     triangleSides = [a, b, c]
     triangleSides.sort()
-    if (triangleSides[2] ** 2) == (triangleSides[1] ** 2) + (triangleSides[0] ** 2):
+    
+    
+    if (triangleSides[2] ** 2) == (triangleSides[1] ** 2) + (triangleSides[0] ** 2): 
         print('Ya did it')
     else:
         print("ya didn't do it")
+        
+        
     print('Would you like to try again? (y/n)')
+    
     x = input()
     while x != 'y' and x != "n":
         print('Would you like to try again? (y/n)')
