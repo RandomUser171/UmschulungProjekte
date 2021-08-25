037b2b0056a2ee1b39d85e728dcae2fb83fe5f82
RandomUser171 <47086691+RandomUser171@users.noreply.github.com>
2021-08-25 18:37:55 +0200

    Update 4. Coin Estimator By Weight.py

--- 4. Coin Estimator By Weight.py
+++ 4. Coin Estimator By Weight.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-@author: Thanasis
-"""
+# This program gives an estimate on the value of coins based on weight.
 
 # (W)eights in grams
 penniesW = 2.5
@@ -16,14 +13,18 @@ nickel = 40
 dime = 50
 quarter = 40
 
-
+# Type of coin being weighed
 def select_coins():
+    
+    
     print('Please enter one of the following type of coins:')
     print('"p" for Pennies')
     print('"n" for Nickels')
     print('"d" for Dimes')
     print('"q" for Quarters')
     x = input('Enter your choice here: ')
+    
+    # Input control
     while x != 'p' and x != 'n' and x != 'd' and x != 'q':
         x = input('Please enter one of the values(p, n, d, q): ')
     if x == 'p':
@@ -36,18 +37,25 @@ def select_coins():
         return quartersW, 'q'
 
 
+    
+ # Weight measurement in grams or pounds
 def select_weight_measurement():
     print('Please enter one of the following type of weight measurement:')
     print('"g" for Grams')
     print('"p" for Pounds')
     x = input('Enter your choice here: ')
+    
+    # Input control
     while x != 'g' and x != 'p':
         x = input('Please enter one of the values(g, p): ')
     return x
 
 
+# Weight input function
 def input_weight():
     xAsInteger = None
+    
+    # Input Control
     while isinstance(xAsInteger, int) == False:
         x = input()
         try:
@@ -57,7 +65,11 @@ def input_weight():
     return xAsInteger
 
 
+
+# Main function
 def questionPrompt():
+    
+    
     a, coinType = select_coins()
     b = select_weight_measurement()
     print('Please enter the weight of the coins')
