f45854d0b01989e99f5441456972fdaba7ff26ef
RandomUser171 <47086691+RandomUser171@users.noreply.github.com>
2021-08-25 18:23:13 +0200

    Update 1. 99 Bottles Of Beer.py

--- 1. 99 Bottles Of Beer.py
+++ 1. 99 Bottles Of Beer.py
@@ -1,12 +1,15 @@
+# This script will print the the sentence "n bottles of beer on the wall, n bottles of beer. Take one down and pass it around, n-1 bottles of beer on the wall."
+# From 100 down to 0
+
 bottles = 'bottles'
 bottles2 = 'bottles'
 n = 100
 for i in range(n, 0, -1):
-    if i == 1:
+    if i == 1: # Makes the var bottles grammatically correct
         bottles = 'bottle'
     else:
         bottles = 'bottles'
-    if i - 1 == 1:
+    if i - 1 == 1: # Makes the var bottles2 grammatically correct
         bottles2 = 'bottle'
     else:
         bottles2 = 'bottles'
