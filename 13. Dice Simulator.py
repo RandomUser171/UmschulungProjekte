bf66b73a54c00ab54c7d1229ebf369c6490e5ef6
RandomUser171 <47086691+RandomUser171@users.noreply.github.com>
2021-08-25 18:34:16 +0200

    Update 13. Dice Simulator.py

--- 13. Dice Simulator.py
+++ 13. Dice Simulator.py
@@ -1,3 +1,5 @@
+# This program allows the user to select the type of dice and the number of times it will be rolled. Afterwards it prints all outcomes and their percentages
+
 import random
 
 def input_def(min,max,print_string):
@@ -26,21 +28,37 @@ def simulate_rolls(sides,rolls):
 
 
 def main():
+    
+    # Main loop
     while True:
         print('Welcome to this dice simulator.\n Please enter how many sides the dice has (3-20) and how many times you want it to be rolled (1-1,000,000)')
+        
+        
         number_of_sides = input_def(3,20,'number of dice\'s sides')
         number_of_rolls = input_def(1,1000000,'number of rolls')
+        
+        
         simulated_rolls = simulate_rolls(number_of_sides,number_of_rolls)
+        
+        
         groupped_simulated_rolls = []
         percentages_of_simulated_rolls = []
+        
+        
         print(simulated_rolls)
+        
+        
         for i in range(1,number_of_sides+1):
             groupped_simulated_rolls.append(simulated_rolls.count(i))
         print(groupped_simulated_rolls)
+        
         for i in range(len(groupped_simulated_rolls)):
             percentages_of_simulated_rolls.append(groupped_simulated_rolls[i]/number_of_rolls)
+            
         for i in range(number_of_sides):
             print('The percentage of the number %d was %%%.2f.'%(i+1,percentages_of_simulated_rolls[i]))
+            
+            
         print('Would you like to play repeat the program? Type "y" to repeat.')
         repeat_program = input()
         if repeat_program.lower() != 'y':
